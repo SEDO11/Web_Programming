b5708572ab5206a095bb1b6cce6e824be4b82050
SEDO11 <do11anm@naver.com>
2021-09-03 18:20:37 +0900

    210903

---
+++ 1week/script.js
@@ -0,0 +1,6 @@
+function show(){
+    document.getElementById("fig").src = "엘비스 프레슬리.jpg"
+}
+function hide(){
+    document.getElementById("fig").src = ""
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Web_Programming
+
+웹 프로그래밍 과목
